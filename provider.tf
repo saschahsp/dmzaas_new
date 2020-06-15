16fd75952725c9610c09c48667c6afd23964d6e2
saschahsp <saschah@gmx.net>
2020-06-15 16:29:38 +0200

    blub

--- provider.tf
+++ provider.tf
@@ -5,9 +5,6 @@ provider "template" {
 provider "oci" {
   version              = "~> 3.78"
   tenancy_ocid         = var.oci_base_identity.tenancy_id
-  user_ocid            = var.oci_base_identity.user_id
-  fingerprint          = var.oci_base_identity.api_fingerprint
-  private_key_path     = var.oci_base_identity.api_private_key_path
   region               = var.oci_base_general.region
   disable_auto_retries = false
 }
